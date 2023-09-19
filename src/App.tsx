33386aee4d920b139a20754b55150fdfa0ef0ac5
Luchian <ionica.luchian@hotmail.com>
2023-09-19 23:34:43 +0300

    form

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"> </header>
+      <main>
+        <form className='login-form'>
+          <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </main>
 
-      </header>
     </div>
   );
 }
